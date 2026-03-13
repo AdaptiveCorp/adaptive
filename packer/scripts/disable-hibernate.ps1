4a76ce0524e184b70e14e44f8568d302a6fb1e26
bric0 <bric0l@proton.me>
2026-03-13 18:30:10 +0100

    WiP pakcer

--- packer/scripts/disable-hibernate.ps1
+++
@@ -1,2 +0,0 @@
-Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\Power\ -Name HiberFileSizePercent -Value 0
-Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\Power\ -Name HibernateEnabled -Value 0
